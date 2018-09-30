0301cc442cb7b2f3fa09dd4c5c2ba44bfdc43b80
LiLiLiLaLa <1921300809@qq.com>
2018-09-30 19:17:24 +0800

    水仙花数

---
+++ main.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+
+int main(void)
+{
+	int a = 0; 
+	int b = 0;
+	int c = 0;
+	int y = 0;
+	int n = 0;
+	int x;
+	for (x = 100; x <= 999; x++)
+	{
+		a = x % 1000 / 100;//求第一位数 
+		b = x % 100 / 10;//求第二位数
+		c = x % 10 / 1;//求第三位数 
+		y = a*a*a + b*b*b + c*c*c;
+		if (y == x)
+		{ 
+			printf("% d", x);
+		    n++;
+		}
+	}
+	printf("\n");
+	printf("水仙花数量为:%d\n", n);
+	system("pause");
+	return 0;
+}
